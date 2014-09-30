400fe2f21eb976f36cbfafa58d6cf4f280ce9ae9
Sarkis Peha <sarkispeha@gmail.com>
2014-09-30 16:34:08 -0600

    quote and author creates DOM elements

--- main.js
+++ main.js
@@ -1,3 +1,16 @@
 $(document).on('ready', function() {
   
+
+$(document).on('click', '.user-submit', function(e) {
+	e.preventDefault();
+	var author = $('input[name="author"').val();
+	var quote = $('input[name="quote"').val();
+	console.log(author);
+	console.log(quote);
+	$('.quotation-area').append('<p>' + author + '</p><p>' + quote + '</p>');
+	$('input[name="author"').val('');
+	$('input[name="quote"').val('');
+
+});
+
 });
