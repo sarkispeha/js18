253f99f7bff6745bfeb7d2555897faebf7f636c2
Sarkis Peha <sarkispeha@gmail.com>
2014-10-01 14:57:32 -0600

    new quotes pushed to quoteLibrary array

--- main.js
+++ main.js
@@ -1,31 +1,55 @@
 $(document).on('ready', function() {
   
-var QuoteLibrary = function(quote) {
-	this.quotes = [];
-}
+/////////////////////////////////////////object classes and variables
+// var QuoteLibrary = function(quote) {
+// 	this.quotes = [];
+// }
+
+var quoteLibrary = [];
 
 var Quote = function(author, quotation) {
 	this.author = author;
 	this.quotation = quotation;
 }
 
+var currentAuthor = '';
+var currentQuote = '';
+ 
+
+//////////////////////////////////////////////events
 
 $(document).on('click', '.user-submit', function(e) {
 	e.preventDefault();
-	var author = $('input[name="author"').val();
-	var quote = $('input[name="quote"').val();
-	// console.log(author);
-	// console.log(quote);
-	$('.quotation-area').append('<div class = "quoteBody"><p>Autor: ' + author + '</p><p>Quotation: "' + quote + '"</p><button class="delete-btn">delete</button></div>');
+	currentAuthor = $('input[name="author"').val();
+	currentQuote = $('input[name="quote"').val();
+	var newQuote = new Quote(currentAuthor, currentQuote);
+	// console.log(newQuote);
+	newQuote.render();
+	/*$('.quotation-area').append('<div class = "quoteBody"><p>Autor: ' + author + '</p><p>Quotation: "' + quote + '"</p><button class="delete-btn">delete</button></div>');
 	$('input[name="author"').val('');
 	$('input[name="quote"').val('');
-
+	make a method that does all this*/
+	quoteLibrary.push(currentQuote);
+	console.log(quoteLibrary);
 });
 
 $(document).on('click', '.delete-btn', function(){
-	console.log($(this));
+	// console.log($(this));
 	$(this).closest('.quoteBody').remove();
-	
 } );
+
+/////////////////////////////////////////////methods
+
+Quote.prototype.render = function() {
+	$('.quotation-area').append('<div class = "quoteBody"><p>Autor: ' + currentAuthor + '</p><p>Quotation: "' + currentQuote + '"</p><button class="delete-btn">delete</button></div>');
+	$('input[name="author"').val('');
+	$('input[name="quote"').val('');
+};
+
+// QuoteLibrary.prototype.pusher = function() {
+// 	this.quotes.push(currentQuote);
+// }
+
+
 
 });
