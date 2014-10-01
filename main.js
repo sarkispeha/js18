b6a1223924c7499c88426444a4583ea726e66912
Sarkis Peha <sarkispeha@gmail.com>
2014-10-01 12:22:34 -0600

    submit and delete buttons appending and removing DOM

--- main.js
+++ main.js
@@ -1,16 +1,31 @@
 $(document).on('ready', function() {
   
+var QuoteLibrary = function(quote) {
+	this.quotes = [];
+}
+
+var Quote = function(author, quotation) {
+	this.author = author;
+	this.quotation = quotation;
+}
+
 
 $(document).on('click', '.user-submit', function(e) {
 	e.preventDefault();
 	var author = $('input[name="author"').val();
 	var quote = $('input[name="quote"').val();
-	console.log(author);
-	console.log(quote);
-	$('.quotation-area').append('<p>' + author + '</p><p>' + quote + '</p>');
+	// console.log(author);
+	// console.log(quote);
+	$('.quotation-area').append('<div class = "quoteBody"><p>Autor: ' + author + '</p><p>Quotation: "' + quote + '"</p><button class="delete-btn">delete</button></div>');
 	$('input[name="author"').val('');
 	$('input[name="quote"').val('');
 
 });
+
+$(document).on('click', '.delete-btn', function(){
+	console.log($(this));
+	$(this).closest('.quoteBody').remove();
+	
+} );
 
 });
